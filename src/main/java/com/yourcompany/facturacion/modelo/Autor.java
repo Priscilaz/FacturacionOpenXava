0d18329d214345227efa3d26ad2ec729f26cf9b2
Priscilaz <priscila.zuniga@udla.edu.ec>
2025-10-26 21:01:00 -0500

    Cambios segun los videos 6,7 y 8

--- src/main/java/com/yourcompany/facturacion/modelo/Autor.java
+++ src/main/java/com/yourcompany/facturacion/modelo/Autor.java
@@ -3,24 +3,15 @@ package com.yourcompany.facturacion.modelo;
 import java.util.*;
 
 import javax.persistence.*;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
 
-import org.hibernate.annotations.*;
 import org.openxava.annotations.*;
 
 import lombok.*;
 
 @Entity
 @Getter @Setter
-public class Autor {
+public class Autor extends Identificable{
 	
-	@Id @GeneratedValue(generator="system-uuid")
-	@Hidden
-	@GenericGenerator(name="system-uuid", strategy="uuid")
-	
-	@Column(length=32)
-	String oid;
 	
 	@Column(length=50)
 	String nombre;
--- src/main/java/com/yourcompany/facturacion/modelo/Factura.java
+++ src/main/java/com/yourcompany/facturacion/modelo/Factura.java
@@ -1,60 +1,27 @@
 package com.yourcompany.facturacion.modelo;
 
-import java.time.*;
 import java.util.*;
 
 import javax.persistence.*;
-import javax.persistence.Entity;
 
-import org.hibernate.annotations.*;
 import org.openxava.annotations.*;
-import org.openxava.calculators.*;
-
-import com.youcompany.facturacion.calculadores.*;
 
 import lombok.*;
 
 @Entity
 @Getter @Setter
-@View(members=
-		"anyo, numero, fecha;" +
-		"cliente;" +
-		"detalles;" +
-		"observaciones"
-		)
-public class Factura {
-	
-	@Id
-	@GeneratedValue(generator = "system-uuid")
-	@Hidden
-	@GenericGenerator(name = "system-uuid", strategy = "uuid")
-	@Column(length=32)	
-	String oid;
+@View(extendsView = "super.DEFAULT", 
+	members = "pedidos{pedidos}"
+      )
+@View(name = "SinClienteNiPedidos",
+		members = 
+			"anyo, numero, fecha;"
+			+ "detalles;"
+			+ "observaciones"
+	 )
+public class Factura extends DocumentoComercial{
 
-	@DefaultValueCalculator(CurrentYearCalculator.class)
-	@Column(length=4)
-	int anyo;
-	
-	
-	@Column(length = 6)
-	@DefaultValueCalculator(value= CalculadorSiguienteNumeroParaAnyo.class,
-	properties = @PropertyValue(name= "anyo"))
-	int numero;
-	
-	@DefaultValueCalculator(CurrentLocalDateCalculator.class)
-	@Required
-	LocalDate fecha;
-	
-	@ManyToOne(fetch = FetchType.LAZY, optional = false)
-	@ReferenceView("Simple")
-	Cliente cliente;
-	
-	@ElementCollection
-	@ListProperties("producto.numero, producto.descripcion,cantidad")
-	Collection<Detalle> detalles;
-	
-	@Stereotype("MEMO")
-	String observaciones;
-	
-	
+	@OneToMany(mappedBy = "factura")
+	@CollectionView("SinClienteNiFactura")
+	Collection<Pedido> pedidos;
 }
